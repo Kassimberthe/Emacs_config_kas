72f1b73bafc963495503b8afa1dbe29d3210c6db
Kassim Berthé <berthekassime@gmail.com>
2025-05-17 17:43:43 +0200

    ajout de raccourci, git-gutter

--- init.el
+++ init.el
@@ -1,20 +1,29 @@
-(org-babel-load-file "~/.emacs.d/config_kas.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-	 '(org-mu4e company-flx which-key web-mode visual-fill-column vertico undo-tree toc-org sudo-edit rainbow-mode rainbow-delimiters pulsar pdf-tools ox-pandoc org-superstar orderless markdown-mode marginalia magit git-timemachine flycheck evil-collection doom-modeline dired-open dired-hide-dotfiles diminish diff-hl crontab-mode consult company-box company-auctex command-log-mode cdlatex catppuccin-theme auto-compile async)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 1.7))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.5))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.4))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.3))))
- '(org-level-6 ((t (:inherit outline-5 :height 1.2))))
- '(org-level-7 ((t (:inherit outline-5 :height 1.1)))))
+
+;;;;;;;;;;;;;;;;;;;;;;; Packages ;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configurer les archives de packages pour MELPA, GNU et Nongnu
+(require 'package)
+
+(setq package-archives
+      '(("gnu"    . "https://elpa.gnu.org/packages/")   ;; Packages officiels GNU
+        ("melpa"  . "https://melpa.org/packages/")      ;; Dernières versions des packages
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/"))) ;; Packages Nongnu
+
+;; Initialiser les archives de packages
+(package-initialize)
+
+;; Mettre à jour les archives si elles ne le sont pas déjà
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Installer use-package si ce n'est pas encore fait
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)  ;; Toujours installer les packages automatiquement
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun package--save-selected-packages (&rest opt) nil)
+(org-babel-load-file (concat user-emacs-directory "config_kas.org"))
+
