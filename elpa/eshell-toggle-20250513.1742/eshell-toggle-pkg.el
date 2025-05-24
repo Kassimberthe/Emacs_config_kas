4d81d77e5ec1a2cfe29c4ff9ea37ca27f94483de
Kassim Berthé <berthekassime@gmail.com>
2025-05-24 16:54:49 +0200

    ajout de shell et terminal

---
+++ elpa/eshell-toggle-20250513.1742/eshell-toggle-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "eshell-toggle" "20250513.1742"
+  "Show/hide eshell under active window."
+  '((emacs "25.1")
+    (dash  "2.11.0"))
+  :url "https://github.com/4da/eshell-toggle"
+  :commit "04e501e02c475bd9067eebcf8807c951f2316194"
+  :revdesc "04e501e02c47"
+  :keywords '("processes")
+  :authors '(("Dmitry Cherkassov" . "dcherkassov@gmail.com"))
+  :maintainers '(("Dmitry Cherkassov" . "dcherkassov@gmail.com")))
